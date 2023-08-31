921456f5aaf70483384106b34e031219abd0a470
Gil-Peixoto <araujo.gil27@gmail.com>
2023-08-31 20:01:39 -0300

    Criado os endpoints de Cliente

--- src/main/java/com/peixoto/institutoHumanizze/controllers/ClienteController.java
+++ src/main/java/com/peixoto/institutoHumanizze/controllers/ClienteController.java
@@ -1,8 +1,60 @@
 package com.peixoto.institutoHumanizze.controllers;
 
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.peixoto.institutoHumanizze.entities.Cliente;
+import com.peixoto.institutoHumanizze.services.ClienteService;
 
 @RestController
+@RequestMapping("/cliente")
 public class ClienteController {
+	
+	@Autowired
+	ClienteService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Cliente>> findAll() {
+		List<Cliente> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Optional<Cliente>> findById(@PathVariable Integer id) {
+		Optional<Cliente> obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Cliente> update(@PathVariable Integer id,@RequestBody Cliente cliente) {
+		Cliente newCliente = service.update(id, cliente);
+		return ResponseEntity.ok().body(newCliente);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Cliente> create(@RequestBody Cliente cliente) {
+		cliente = service.create(cliente);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(cliente.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Cliente> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 
 }
--- src/main/java/com/peixoto/institutoHumanizze/services/ClienteService.java
+++ src/main/java/com/peixoto/institutoHumanizze/services/ClienteService.java
@@ -1,8 +1,45 @@
 package com.peixoto.institutoHumanizze.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.peixoto.institutoHumanizze.entities.Cliente;
+import com.peixoto.institutoHumanizze.repositories.ClienteRepository;
 
 @Service
 public class ClienteService {
+	
+	@Autowired
+	ClienteRepository repository;
+	
+	public List<Cliente> findAll() {
+		List<Cliente> list = repository.findAll();
+		return list;
+	}
+	
+	public Optional<Cliente> findById(Integer id) {
+		Optional<Cliente> obj = repository.findById(id);
+		return obj;
+	}
+	
+	public Cliente update(Integer id, Cliente cliente) {
+		findById(id);
+		cliente.setId(id);
+		return repository.save(cliente);
+	}
+	
+	public Cliente create(Cliente cliente) {
+		cliente.setId(null);
+		return repository.save(cliente);
+	}
+	
+	public void delete(Integer id) {
+		repository.deleteById(id);
+	}
+
+	
 
 }
