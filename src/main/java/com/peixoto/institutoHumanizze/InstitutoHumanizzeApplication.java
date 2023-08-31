ed004954d378823a8a931a2e8cefce3ff296d8a2
Gil-Peixoto <araujo.gil27@gmail.com>
2023-08-31 17:22:17 -0300

    Projeto criado

---
+++ src/main/java/com/peixoto/institutoHumanizze/InstitutoHumanizzeApplication.java
@@ -0,0 +1,13 @@
+package com.peixoto.institutoHumanizze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstitutoHumanizzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstitutoHumanizzeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peixoto/institutoHumanizze/InstitutoHumanizzeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peixoto.institutoHumanizze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstitutoHumanizzeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
