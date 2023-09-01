b7e484e0dab426fecb986e7a4c097774c045b3cb
Gil-Peixoto <araujo.gil27@gmail.com>
2023-09-01 15:29:23 -0300

    Classe AgendamentoService e AgendamentoController criada

---
+++ src/main/java/com/peixoto/institutoHumanizze/controllers/AgendamentoController.java
@@ -0,0 +1,10 @@
+package com.peixoto.institutoHumanizze.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/agendamento")
+public class AgendamentoController {
+
+}
---
+++ src/main/java/com/peixoto/institutoHumanizze/services/AgendamentoService.java
@@ -0,0 +1,8 @@
+package com.peixoto.institutoHumanizze.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AgendamentoService {
+
+}
